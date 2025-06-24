3e188b72d466b9a9e859fd431eac026723000db3
imalbi <alberto.cerqua@icloud.com>
2025-06-24 12:06:47 +0200

    Init offers page, logic on filters, added tailwind config

--- src/components/loginForm.svelte
+++ src/components/loginForm.svelte
@@ -8,24 +8,24 @@
 </script>
 
 <div class="bg-[url(/media/login2_bg.png)] bg-cover">
-						<form class="flex flex-col justify-end h-180 gap-4 w-7/10 mx-auto" method="POST" action="?/login">
-							<h1 class="text-center text-2xl font-bold">Accedi</h1>
-							<input type="hidden" name="redirectTo" value={data.redirectTo || '/account'} />
-							<input 
-								class="w-full rounded-lg border border-gray-300 px-4 py-2 focus:outline-none focus:ring-2 focus:ring-amber-500" 
-								type="email" 
-								placeholder="email" 
-								name="email"
-								required>
-							
-							<input 
-								class="w-full rounded-lg border border-gray-300 px-4 py-2 focus:outline-none focus:ring-2 focus:ring-amber-500" 
-								type="password" 
-								placeholder="password" 
-								name="password"
-								required>
-                            <button class="w-full h-10 bg-amber-600 rounded-2xl text-white cursor-pointer" type="submit">Login</button>
-							<button class="w-full h-10 bg-amber-600 rounded-2xl text-white cursor-pointer" type="submit">Register</button>
-						</form>
-					</div>
+	<form class="flex flex-col justify-end h-180 gap-4 w-7/10 mx-auto" method="POST" action="?/login">
+		<h1 class="text-center text-2xl font-bold">Accedi MockData</h1>
+		<input type="hidden" name="redirectTo" value={data.redirectTo || '/account'} />
+		<input 
+			class="w-full rounded-lg border border-gray-300 px-4 py-2 focus:outline-none focus:ring-2 focus:ring-amber-500" 
+			type="email" 
+			placeholder="email" 
+			name="email"
+			required>
+		
+		<input 
+			class="w-full rounded-lg border border-gray-300 px-4 py-2 focus:outline-none focus:ring-2 focus:ring-amber-500" 
+			type="password" 
+			placeholder="password" 
+			name="password"
+			required>
+		<button class="w-full h-10 bg-amber-600 rounded-2xl text-white cursor-pointer" type="submit">Login</button>
+		<button class="w-full h-10 bg-amber-600 rounded-2xl text-white cursor-pointer" type="submit">Register</button>
+	</form>
+</div>
 
---
+++ src/routes/(protected)/offers/+page.svelte
@@ -0,0 +1,44 @@
+<script>
+    const filters = [
+        {type:"tutti", label:"Tutte"},
+        {type:"cheap", label:"Offerte <=1€"},
+        {type:"carne", label:"Carne"},
+        {type:"pollo", label:"Pollo"},
+        {type:"snack", label:"Snack"},
+        {type:"bevande", label:"Bevande"},
+        {type:"combo", label:"Combo"},
+        {type:"glutenfree", label:"Senza Glutine"}
+    ]
+    import HomeCard from "../../../components/homeCard.svelte";
+    let activeFilter=$state('tutti');
+    function handleClick(filterType){
+        console.log('Filtro selezionato:', filterType); //debug
+        activeFilter = filterType;
+    }
+    
+</script>
+
+<main class="w-full m-auto mt-3">
+    <h1 class="w-10/12 m-auto font-bold text-2xl p-2">Offerte</h1>
+    <div class="flex  items-center overflow-x-scroll gap-2 scroll-auto scrollbar-hide mx-1">
+
+        {#each filters as filter(filter.type) }
+
+            <button class=" px-2 py-1 border-gray-800 border rounded-3xl whitespace-nowrap transition-colors" 
+             onclick={() => handleClick(filter.type)}
+            class:bg-amber-400={activeFilter===filter.type}
+            >{filter.label}</button>
+
+        {/each}
+
+    </div>
+
+    <div>
+
+    </div>
+
+</main>
+
+<style>
+
+</style>
